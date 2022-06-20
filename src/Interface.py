b97d649231ffab51f1bc790537a17313ea171ad6
Maicon <mz_maicon@hotmail.com>
2022-06-20 20:01:41 -0300

    Adicionado arrastar e soltar

---
+++ src/Interface.py
@@ -0,0 +1,47 @@
+import PySimpleGUIQt as sg
+from Services.Exportador import Exportador
+
+from Services.Importador import Importador
+
+class Listbox(sg.Listbox):
+
+    def dragEnterEvent(self, e):
+        e.accept()
+
+    def dragMoveEvent(self, e):
+        e.accept()
+
+    def dropEvent(self, e):
+        #data = window['LISTBOX'].get_list_values()
+        #print(data)
+        # items = [str(v) for v in e.mimeData().text().strip().split('\n')]
+        # print(items)
+        # data.extend(items)
+        # print(data)
+        data = e.mimeData().text().strip().split('\n')
+        data[0] = data[0].replace('file:///', '')
+        window['LISTBOX'].update(data)
+        window.refresh()
+        dados = Importador.ImportarComoCsv(self, path="C:\Users\mz_ma\Documents\Projetos\cc\CityLivingLab\pessoal.csv")
+        Exportador.ExportarComoJson(self, dados)
+        print("Feito!")
+
+    def enable_drop(self):
+        # Called after window finalized
+        self.Widget.setAcceptDrops(True)
+        self.Widget.dragEnterEvent = self.dragEnterEvent
+        self.Widget.dragMoveEvent = self.dragMoveEvent
+        self.Widget.dropEvent = self.dropEvent
+
+layout = [[Listbox([], size=(50, 4), enable_events=True, key='LISTBOX')]]
+
+window = sg.Window("Title", layout, finalize=True)
+window['LISTBOX'].enable_drop()
+
+while True:
+    event, values = window.read()
+    if event == sg.WINDOW_CLOSED:
+        break
+
+
+window.close()
--- src/Programa.py
+++ src/Programa.py
@@ -5,12 +5,11 @@ class Programa():
 	def __init__(self):
 		url1 = 'https://remuneracoes.caxias.rs.gov.br/2022/05/01/mensal'
 		url2 = 'https://www.w3schools.com/html/html_tables.asp'
-		dados = Importador.ImportarComoCsv(self, path='../pessoal.csv')
+		#dados = Importador.ImportarComoCsv(self, path='../pessoal.csv')
 		#dados = Importador.ImportarComoHtml(self, url=url2)
 		#dados = Importador.ImportarComoPdf(self, '../salario.pdf')
-		Exportador.ExportarComoJson(self, dados)
-		print('Feito!')
-
+		# Exportador.ExportarComoJson(self, dados)
+		# print('Feito!')
 
 if __name__ == "__main__":
     Programa()
